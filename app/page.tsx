a30d21e1f6e6908190e9db97a4da2c1c9ad0ef58
msholehhudin <msholehhudin.ms@gmail.com>
2024-07-03 18:11:43 +0700

    Hero and About section Done

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
+import Image from "next/image";
+import { FaHome } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        {/* <h1>Hello, Nadlyne !</h1> */}
+        <FloatingNav navItems={navItems} />
+        {/* <h1>Helloo</h1> */}
+        <Hero />
+        {/* <h1>Helloo</h1>
+        <h1>Helloo</h1>
+        <h1>Helloo</h1> */}
+        <Grid />
+      </div>
+    </main>
+  );
+}
