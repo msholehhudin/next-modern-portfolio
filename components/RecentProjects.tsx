177438ba48583206fc54d276b86f4a5b83144fef
msholehhudin <msholehhudin.ms@gmail.com>
2024-07-16 19:18:44 +0700

    Recent Proejct Updated

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -11,14 +11,23 @@ const RecentProjects = () => {
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10 border border-yellow-400">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
-          <div className="border border-red-500 lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
+          <div
+            className="border border-red-500 lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            key={id}
+          >
             <PinContainer title={title} href={link}>
-              <div className="flex items-center justify-center w-[80vw] h-[20vh] sm:w-96">
-                <div className="">
+              <div className="relative flex items-center justify-center w-[80vw] h-[20vh] sm:w-96 overflow-hidden lg:h-[30vh] mb-10">
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
-                <img src={img} alt={title} />
+                <img src={img} alt={title} className="z-10 absolute bottom-0" />
               </div>
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {title}
+              </h1>
+              <p className="text-sm lg:text-xl lg:font-normal font-light line-clamp-2">
+                {des}
+              </p>
             </PinContainer>
           </div>
         ))}
