9b03090a6e6decc3e473d3deb8a39836d6ed38f8
msholehhudin <msholehhudin.ms@gmail.com>
2024-07-23 06:38:59 +0700

    feat: :zap: Client section done

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -73,7 +73,7 @@ export const InfiniteMovingCards = ({
     <div
       ref={containerRef}
       className={cn(
-        "scroller relative z-20  max-w-7xl overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        "scroller relative z-20  w-screen overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
         className
       )}
     >
@@ -87,28 +87,36 @@ export const InfiniteMovingCards = ({
       >
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[60vw]"
+            className="w-[90vw] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-800 p-5 md:p-16 md:w-[60vw]"
             style={{
+              // background: "rgb(4,7,29)",
               background:
                 "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
+              // backgroundColor:
+              //   "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(57,85,177,0.6306897759103641) 41%, rgba(0,212,255,1) 50%)",
             }}
-            key={item.name}
+            key={idx}
           >
             <blockquote>
               <div
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-              <span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
+              <span className=" relative z-20 text-lg leading-[1.6] text-white font-normal">
                 {item.quote}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
-                <span className="flex flex-col gap-1">
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.name}
-                  </span>
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.title}
+                <span className="flex gap-1">
+                  <div className="me-3">
+                    <img src="/profile.svg" alt="profile" />
+                  </div>
+                  <span className="flex flex-col">
+                    <span className=" text-xl leading-[1.6] text-white font-bold">
+                      {item.name}
+                    </span>
+                    <span className=" text-sm leading-[1.6] text-white-200 font-normal">
+                      {item.title}
+                    </span>
                   </span>
                 </span>
               </div>
